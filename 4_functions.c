b7640f841ba2ffa4da5579670b563fd3a5b0be68
Daniel Nyakundi <nyaxda@gmail.com>
2023-11-13 10:20:10 -0500

    Add function to print character as uppercase hexadecimal

--- 4_functions.c
+++ 4_functions.c
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * print_char_hex - prints a character in uppercase hexadecimal
+ * 
+ * @c: character to print
+ * 
+ * Return: number of characters printed
+ */
+void print_char_as_hex(unsigned char c)
+{
+    char *hex_digits = "0123456789ABCDEF";
+    write(1, &hex_digits[c / 16], 1);
+    write(1, &hex_digits[c % 16], 1);
+}
+
+/**
  * print_custom_string - prints a string except for non-printable characters
  *
  * Description: prints a string except for non-printable characters.
@@ -26,7 +40,7 @@ int print_custom_string(va_list args)
 		if (*s < 32 || *s >= 127)
 		{
 			write (1, "\\x", 2);
-			print_uppercase_hexadecimal(*s);
+			print_char_as_hex((unsigned char)*s);
 			len += 4;
 
 		}
